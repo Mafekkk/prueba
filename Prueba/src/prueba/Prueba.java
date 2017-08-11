698c3f75a82192a01fe985afd4942ca1b1326e0f
Estudiantes <Estudiantes@labing50418.udistrital.edu.co>
2017-08-11 23:24:30 +0200

    Bienvenidos a mi página web, ahora le puse hola mundo, se adiciona la funcionalidad de imprimir

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -4,6 +4,10 @@ package prueba;
 public class Prueba {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+//        System.out.println("Hola Mundo");
+        int i;
+        for(i=0; i<100;i++){
+            System.out.println("Hola mundo");
+        }
     }
 }
